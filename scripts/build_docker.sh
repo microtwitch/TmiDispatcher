ee7606df4f27214ed764286c65b022c9e31c0129
M4tthewDE <mattider2@gmail.com>
2021-12-27 23:56:25 +0100

    add docker hub action

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gradle clean && gradle build jar && sudo docker build --build-arg JAR_FILE=build/libs/\*.jar -t microtwitch/tmi-dispatcher .
+gradle clean && gradle build jar && sudo docker build --build-arg JAR_FILE=build/libs/\*.jar -t m4tthewde/tmi-dispatcher .
